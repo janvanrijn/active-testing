d852707730a2c7b31772c732688639437b18b481
janvanrijn@gmail.com <janvanrijn@gmail.com>
2017-09-20 19:44:34 +0200

    surrogate evaluation

--- active-testing/eval/surrogate.py
+++ active-testing/eval/surrogate.py
@@ -1,8 +1,14 @@
 import argparse
 import json
+import numpy as np
 import openml
 import os
+import pandas as pd
 import pickle
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
 
 
 def parse_args():
@@ -12,7 +18,7 @@ def parse_args():
     parser.add_argument('--study_id', type=str, default='OpenML100', help='the tag to obtain the tasks from')
     parser.add_argument('--flow_id', type=int, default=6952, help='openml flow id')
     parser.add_argument('--relevant_parameters', type=json.loads,
-                        default='{"C": 0, "gamma": 0, "kernel": 0, "coef0": 0, "tol": 0}')
+                        default='{"C": "numeric", "gamma": "numeric", "kernel": "categorical", "coef0": "numeric", "tol": "numeric"}')
     parser.add_argument('--openml_server', type=str, default=None, help='the openml server location')
     parser.add_argument('--openml_apikey', type=str, default=None, help='the apikey to authenticate to OpenML')
     return parser.parse_args()
@@ -22,6 +28,8 @@ if __name__ == '__main__':
     args = parse_args()
     cache_directory = args.cache_directory + '/' + str(args.flow_id)
     study_cache_path = cache_directory + '/study.pkl'
+    num_runs = 500
+    all_scores = []
     try:
         os.makedirs(cache_directory)
     except FileExistsError:
@@ -48,7 +56,7 @@ if __name__ == '__main__':
         setups_cache_path = cache_directory + '/' + str(task_id) + '/setups.pkl'
         if not os.path.isfile(evaluations_cache_path) or not os.path.isfile(setups_cache_path):
             study = openml.study.get_study(args.study_id, 'tasks')
-            evaluations = openml.evaluations.list_evaluations('predictive_accuracy', size=200, task=[task_id],
+            evaluations = openml.evaluations.list_evaluations('predictive_accuracy', size=num_runs, task=[task_id],
                                                               flow=[args.flow_id])
             with open(evaluations_cache_path, 'wb') as fp:
                 pickle.dump(evaluations, fp)
@@ -84,15 +92,32 @@ if __name__ == '__main__':
                 hyperparameters[name] = value
             setup_parameters[setup_id] = hyperparameters
 
-        X = []
         y = []
+        dataframe = pd.DataFrame(columns=relevant_parameters)
+        categoricals = set()
+        for idx, param in enumerate(relevant_parameters):
+            if args.relevant_parameters[param]:
+                categoricals.add(idx)
+
         for run_id, evaluation in evaluations.items():
-            currentX = []
-            for param in relevant_parameters:
-                currentX.append(setup_parameters[evaluation.setup_id][param])
+            currentX = {}
+            for idx, param in enumerate(relevant_parameters):
+                currentX[param] = json.loads(setup_parameters[evaluation.setup_id][param])
 
-            X.append(currentX)
-            y.append(evaluation.value)
+            dataframe = dataframe.append(currentX, ignore_index=True)
+            y.append(float(evaluation.value))
+        X = np.array(pd.get_dummies(dataframe).as_matrix())
+        y = np.array(y)
 
-        print(X)
-        print(y)
+        if X.shape[0] > num_runs:
+            raise ValueError()
+        if y.shape[0] > num_runs:
+            raise ValueError()
+
+        clf = RandomForestRegressor()
+        scores = cross_val_score(clf, X, y, cv=10, scoring='neg_mean_absolute_error')
+        all_scores.append(scores.mean())
+        print("Task %d; Ranges: [%2f-%2f]; MSE: %0.4f (+/- %0.4f)" % (task_id, min(y), max(y), scores.mean(), scores.std() * 2))
+
+        # TODO: correlation coefficient :)
+    print("ALL: ", np.mean(all_scores))
